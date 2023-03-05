4799838583934dc37e0b0a1d613761addd65ae36
emmanueln4ssif <emmanuel.nassif@gmail.com>
2023-03-05 14:10:03 -0300

    Adicionando logomarca do sistema

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="https://o.remove.bg/downloads/7535e7ab-daeb-4e90-928d-7b896530bdce/image-removebg-preview.png" width="70px" alt="Logo">
+<img src="{{ asset('img/logo_acad.png') }}" width="100px" alt="Logo">
